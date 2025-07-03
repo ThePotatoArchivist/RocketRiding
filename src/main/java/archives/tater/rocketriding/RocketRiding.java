80e95f35c3ca83c6182cce4fef98eda0c63ac17c
ThePotatoArchivist <josiahfu@gmail.com>
2025-07-03 23:13:55 +0900

    add to loot tables, change enchantment id to snake case, remove redundant exclusion tag

--- src/main/java/archives/tater/rocketriding/RocketRiding.java
+++ src/main/java/archives/tater/rocketriding/RocketRiding.java
@@ -3,6 +3,7 @@ package archives.tater.rocketriding;
 import archives.tater.rocketriding.mixin.EnchantmentInvoker;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.component.ComponentType;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ItemEnchantmentsComponent;
@@ -14,9 +15,15 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.LootTables;
 import net.minecraft.loot.context.LootContextTypes;
+import net.minecraft.loot.entry.LootTableEntry;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Identifier;
@@ -38,6 +45,9 @@ public class RocketRiding implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final RegistryKey<LootTable> END_CITY_INJECT = RegistryKey.of(RegistryKeys.LOOT_TABLE, id("inject/end_city_treasure"));
+	public static final RegistryKey<LootTable> TRIAL_CHAMBER_INJECT = RegistryKey.of(RegistryKeys.LOOT_TABLE, id("inject/trial_chambers"));
 
 	private static <T> ComponentType<T> register(Identifier id, UnaryOperator<ComponentType.Builder<T>> builderOperator) {
 		return Registry.register(Registries.ENCHANTMENT_EFFECT_COMPONENT_TYPE, id, (builderOperator.apply(ComponentType.builder())).build());
@@ -78,5 +88,20 @@ public class RocketRiding implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		Registry.register(Registries.ENCHANTMENT_ENTITY_EFFECT_TYPE, id("owner_mount"), OwnerMountEnchantmentEntityEffect.CODEC);
+
+		LootTableEvents.MODIFY.register((registryKey, builder, source, wrapperLookup) -> {
+            if (!source.isBuiltin()) return;
+            if (registryKey == LootTables.END_CITY_TREASURE_CHEST)
+				builder.pool(LootPool.builder()
+						.with(LootTableEntry.builder(END_CITY_INJECT))
+						.build());
+			if (registryKey == LootTables.TRIAL_CHAMBERS_REWARD_RARE_CHEST || registryKey == LootTables.TRIAL_CHAMBERS_REWARD_OMINOUS_RARE_CHEST)
+				builder.modifyPools(poolBuilder ->
+					poolBuilder.with(
+							LootTableEntry.builder(TRIAL_CHAMBER_INJECT)
+									.weight(2)
+					)
+				);
+        });
 	}
 }
